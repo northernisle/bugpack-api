d6145467c88ab1129003da4ed065ff23c29be53b
Albert Gadjaev <gadjaev.albert@gmail.com>
2019-12-23 15:35:58 +0200

    Add some integration tests against the /user route

--- src/tests/api/user.test.ts
+++ src/tests/api/user.test.ts
@@ -13,11 +13,14 @@ test('Should create a new user', async () => {
   const email = 'hammerlock@gentlemen.com'
   const password = 'hunter-scholar-gentleman';
 
-  const res = await request(app).post('/users').send({
-    name,
-    email,
-    password
-  }).expect(201);
+  const res = await request(app)
+    .post('/users')
+    .send({
+      name,
+      email,
+      password
+    })
+    .expect(201);
 
   const user = await User.findById(res.body.user._id);
   expect(user).not.toBeNull();
@@ -31,4 +34,89 @@ test('Should create a new user', async () => {
   });
 
   expect(user?.password).not.toBe(password);
+});
+
+test('Should sign in existing user', async () => {
+  const res = await request(app)
+    .post('/users/login')
+    .send({
+      email: dbUser.email,
+      password: dbUser.password
+    })
+    .expect(200);
+
+  const user = await User.findById(dbUserId);
+  expect(user).not.toBeNull();
+
+  expect(res.body.token).toBe(user?.tokens[1].token);
+});
+
+test('Should NOT sign in wrong credentials', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: 'katagawa@maliwan.com',
+      password: 'Zanara'
+    }).expect(400);
+});
+
+test('Should sign out an authenticated user', async () => {
+  await request(app)
+    .get('/users/logout')
+    .set('Authorization', `Bearer ${dbUser.tokens[0].token}`)
+    .expect(200);
+
+  const user = await User.findById(dbUserId);
+  expect(user?.tokens[0]).toBeUndefined();
+});
+
+test('Should NOT sign out an unauthenticated user', async () => {
+  await request(app)
+    .get('/users/logout')
+    .expect(401);
+});
+
+test('Should return the data for an authenticated user', async () => {
+  const res = await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${dbUser.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(res.body).toMatchObject({
+    _id: dbUser._id.toHexString(),
+    name: dbUser.name,
+    email: dbUser.email,
+  });
+});
+
+test('Should NOT return the data for an unauthenticated user', async () => {
+  const res = await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401);
+
+  expect(res.body).toMatchObject({});
+});
+
+test('Should return the unavailability of an occupied email', async () => {
+  const res = await request(app)
+    .post('/users/emailOccupied')
+    .send({
+      email: dbUser.email
+    })
+    .expect(200);
+
+  expect(res.body).toBe(true);
+});
+
+test('Should return the availability of an email', async () => {
+  const res = await request(app)
+    .post('/users/emailOccupied')
+    .send({
+      email: 'hammerlock@gentlemen.com'
+    })
+    .expect(200);
+
+  expect(res.body).toBe(false);
 });
